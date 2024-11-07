90f9f40d16c50dcdaec5b71c0b0faf8b725c06bf
joeprz <perez_jr4@hotmail.com>
2024-11-07 14:59:01 -0600

    my api creado

---
+++ practicas/actividades/act7/backend/myapi/DataBase.php
@@ -0,0 +1,20 @@
+<?php
+namespace MyApi;
+abstract class DataBase {
+    protected $conexion = NULL;
+    
+    public function __construct($user,$pass,$db) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+
+        // Si la conexión falló contendrá false
+        if (!$this->conexion) {
+            die('Error de conexión (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+        }
+    }
+}
+?>
